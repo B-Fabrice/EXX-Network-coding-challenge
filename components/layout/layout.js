c98602388d262a44bb34b2c2fd519ec045bd9721
B-Fabrice <bizimanafabrice707@gmail.com>
2022-12-25 10:45:54 +0200

    register footer to main

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types'
 import Navbar from 'components/cards/navbar'
+import Footer from 'components/cards/footer'
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </>
   )
 }
