4f5bdb84fb0bfffeb20db533cd4094b75dfa769c
B-Fabrice <bizimanafabrice707@gmail.com>
2022-12-25 14:41:44 +0200

    text sizing

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className='mt-10'>
       <div className='text-center'>
-        <h1 className='font-bold text-5xl px-10'>
+        <h1 className='font-bold md:text-5xl text-4xl px-10 leading-normal'>
           Start building apps with
-          <span className='text-primary ml-2'>useful cases</span>
+          <span className='text-primary ml-2 md:inline-block block'>useful cases</span>
         </h1>
 
-        <p className='mt-10 max-w-md mx-auto px-10'>
+        <p className='mt-10 max-w-xl mx-auto px-10 text-gray-800 text-lg md:text-xl'>
           Welcome to the EXX Developer’s Guide. Now you can begin building your first DApp on EXX.
         </p>
 
@@ -77,8 +77,8 @@ export default function Home() {
         <div className='relative'>
           <div className='md:hidden bg-slate-100 absolute top-0 right-0 left-0 bottom-0 -z-20'></div>
           <div className='bg-primary absolute top-0 right-0 left-0 bottom-2/3 md:bottom-[30px] -z-10'></div>
-          <h2 className='text-white font-bold text-4xl py-20'>Your journey to developing your first dapp.</h2>
-          <div className='bg-white rounded-3xl mx-10 flex flex-col md:flex-row justify-between max-w-5xl md:mx-auto p-7 shadow-md'>
+          <h2 className='text-white font-bold md:text-4xl px-10 text-left md:text-center text-3xl py-20'>Your journey to developing your first dapp.</h2>
+          <div className='bg-white rounded-3xl mx-5 flex flex-col md:flex-row justify-between max-w-5xl md:mx-auto p-7 shadow-md'>
             {[
               { src: '/dev_icon1.png', title: 'Write Smart Contract', desc: 'Write instructions via code on the Exx network.', instry: '{solidity}' },
               { src: '/dev_icon2.png', title: 'Build Web Apps', desc: 'Build a frontend application for easier interaction', instry: '{next} {react}' },
@@ -91,8 +91,8 @@ export default function Home() {
                   width={56}
                   height={56}
                 />
-                <h2 className='text-2xl font-bold mb-5 mt-4 text-black'>{item.title}</h2>
-                <p className='text-gray-700'>
+                <h2 className='text-xl md:text-2xl font-bold mb-5 mt-4 text-black'>{item.title}</h2>
+                <p className='text-gray-700 text-lg md:text-xl'>
                   {item.desc}<br />
                   {item.instry}
                 </p>
@@ -102,14 +102,14 @@ export default function Home() {
           </div>
         </div>
 
-        <div className='p-24 flex flex-col md:flex-row gap-20 md:mt-24 mx-auto max-w-5xl md:rounded-3xl bg-slate-100'>
+        <div className='md:p-24 px-10 py-24 flex flex-col md:flex-row gap-20 md:mt-24 mx-auto max-w-5xl md:rounded-3xl bg-slate-100'>
           <div className='flex-1'>
-            <h2 className='text-4xl font-bold'>Get connected to the EXX Network</h2>
+            <h2 className='text-3xl md:text-4xl font-bold'>Get connected to the EXX Network</h2>
             <div className='flex flex-col md:flex-row gap-5 mt-12'>
-              <button className='bg-primary text-white px-5 py-4 rounded-lg w-full block'>
+              <button className='whitespace-nowrap bg-primary text-white px-5 py-4 rounded-lg w-full block'>
                 Add to Metamask
               </button>
-              <button className='border-secondary px-5 py-4 rounded-lg block w-full border-2'>
+              <button className='whitespace-nowrap border-secondary px-5 py-4 rounded-lg block w-full border-2'>
                 Learn More
               </button>
             </div>
@@ -122,8 +122,9 @@ export default function Home() {
               { key: 'Chain ID:', value: '47' },
               { key: 'Block Explorer:', value: 'https://exxscan.com' },
             ].map((item, index) => (
-              <div key={index}>
-                <span className='text-gray-600 font-light mr-2'>{item.key}</span>{item.value}
+              <div key={index} className='text-lg md:text-xl'>
+                <span className='text-gray-600 font-light mr-2'>{item.key}</span>
+                {item.value}
               </div>
             ))
             }
@@ -137,14 +138,14 @@ export default function Home() {
             { title: 'Blockchain Explorer', desc: 'Explore transactions on Exx', link_name: 'Explore now', link: '#' },
           ].map((item, index) => (
             <div className='text-left py-5 px-6' key={index}>
-              <h2 className='text-xl'>
+              <h2 className='md:text-2xl text-xl'>
                 <span className='md:block pr-2 font-bold text-secondary'>0{index + 1}</span>
                 <span className='md:block font-bold mb-5 mt-4 text-black'>{item.title}</span>
               </h2>
-              <p className='text-gray-500'>{item.desc}</p>
+              <p className='text-gray-500 text-lg md:text-xl'>{item.desc}</p>
               <Link
                 href={item.link}
-                className='mt-2 block text-secondary underline'
+                className='mt-2 block text-secondary underline text-lg md:text-xl'
               >
                 {item.link_name}
               </Link>
@@ -153,10 +154,10 @@ export default function Home() {
           }
         </div>
 
-        <div className='mt-20 rounded-3xl overflow-hidden mx-auto md:max-w-5xl flex flex-col md:flex-row bg-black'>
-          <div className='text-left flex flex-col p-10 gap-10 md:max-w-md'>
-            <h2 className='text-3xl block font-bold text-white'>Join our developer community</h2>
-            <p className='text-gray-200'>
+        <div className='mt-20 rounded-3xl overflow-hidden  mx-3 md:mx-auto md:max-w-5xl flex flex-col md:flex-row bg-black'>
+          <div className='text-left flex flex-col p-5 md:p-10 gap-10 md:max-w-md'>
+            <h2 className='md:text-5xl text-4xl block font-bold text-white'>Join our developer community</h2>
+            <p className='text-gray-400 md:text-xl text-lg'>
               Join our community of developers from diverse backgrounds where you can work collaboratively, develop projects, ask questions and grow your career.
             </p>
             <div>
@@ -167,35 +168,43 @@ export default function Home() {
             <Image
               src='/card_bg.png'
               alt='icon'
-              width={100}
-              height={100}
-              className='md:h-full md:w-fit w-full h:fit'
+              width={500}
+              height={500}
+              className='hidden md:block h-full w-full'
+            />
+
+            <Image
+              src='/mbl_card_bg.png'
+              alt='icon'
+              width={500}
+              height={500}
+              className='md:hidden h-full w-full'
             />
           </div>
         </div>
 
         <div className='mt-20 md:px-0 px-10 text-left max-w-5xl mx-auto'>
-          <h2 className='text-4xl font-bold'>Exx Updates</h2>
-          <p className='max-w-md mt-10 text-gray-600'>
+          <h2 className='md:text-4xl text-3xl font-bold'>Exx Updates</h2>
+          <p className='max-w-md text-lg md:text-xl mt-10 text-gray-600'>
             Catch up with news, blog posts, events and other happenings within the EXX ecosystem.
           </p>
           <div className='flex flex-col md:flex-row gap-5 justify-between'>
             {[
-              {src: '/card_img1.png'},
-              {src: '/card_img2.png'},
-              {src: '/card_img3.png'},
+              { src: '/card_img1.png' },
+              { src: '/card_img2.png' },
+              { src: '/card_img3.png' },
             ].map((item, index) => (
               <div className='bg-slate-100 md:bg-gray-100 mt-10 rounded-xl' key={index}>
-                <p className='inline-flex py-3 px-4 w-full justify-between'>
-                  <span className='inline-block font-bold text-sm'>Blockchain tips</span>
-                  <span className='inline-block font-light text-sm'>March 27, 2022</span>
+                <p className='inline-flex py-3 px-4 w-full text-xs justify-between'>
+                  <span className='inline-block font-bold'>Blockchain tips</span>
+                  <span className='inline-block font-light'>March 27, 2022</span>
                 </p>
-                <h2 className='mb-6 px-4 text-md font-bold'>The design language of the Cross Chain- pekele pekele</h2>
+                <h2 className='mb-6 px-4 md:text-lg text-base font-bold'>The design language of the Cross Chain- pekele pekele</h2>
                 <Image
                   src={item.src}
                   alt='icon'
-                  width={100}
-                  height={100}
+                  width={500}
+                  height={500}
                   className='w-full h-fit'
                 />
               </div>
@@ -204,10 +213,10 @@ export default function Home() {
           </div>
         </div>
 
-        <div className='mt-20 bg-black flex md:gap-0 gap-10 flex-col md:flex-row px-10 md:px-16 text-left py-24 text-white'>
+        <div className='mt-20 bg-black flex md:gap-0 gap-10 flex-col md:flex-row px-5 md:px-16 text-left py-24 text-white'>
           <div className='w-full'>
-            <h2 className='text-3xl font-bold'>Stay Updated</h2>
-            <p className='max-w-xs mt-10'>
+            <h2 className='md:text-4xl text-3xl font-bold'>Stay Updated</h2>
+            <p className='max-w-xs text-lg md:text-xl mt-5'>
               Subscribe to our newsletter. We only send important updates.
             </p>
           </div>
