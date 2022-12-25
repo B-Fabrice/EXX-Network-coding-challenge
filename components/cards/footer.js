f68fb8bc9c9d2535b36720ead2ac759a20b15bb6
B-Fabrice <bizimanafabrice707@gmail.com>
2022-12-25 14:40:07 +0200

    footer sizing

--- components/cards/footer.js
+++ components/cards/footer.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Footer() {
@@ -5,21 +6,27 @@ export default function Footer() {
     <footer className='py-16 px-10 md:px-14'>
       <div className='flex md:justify-start justify-between flex-wrap md:flex-nowrap gap-10'>
         <div className='md:max-w-xs md:min-w-[300px] w-full'>
-          <h2 className='text-primary text-xl font-bold'>EXX Blockchain</h2>
-          <p className='text-gray-800 md:max-w-none max-w-xs py-4'>EXX builds on the efficacy of Ethereum, it is faster, powerful and more secured</p>
+          <Image
+            src='/logo.png'
+            width={100}
+            height={100}
+            className=''
+            alt='logo'
+          />
+          <p className='text-gray-800 text-lg md:text-xl md:max-w-none max-w-xs py-4'>EXX builds on the efficacy of Ethereum, it is faster, powerful and more secured</p>
         </div>
         <div className='md:w-full w-1/3'>
-          <h2 className='text-xl font-bold'>General</h2>
+          <h2 className='md:text-2xl text-xl font-bold'>General</h2>
           {[
-            {name: 'About'},
-            {name: 'Contact us'},
-            {name: 'Brand Kits'},
-            {name: 'News & Updates'},
+            { name: 'About' },
+            { name: 'Contact us' },
+            { name: 'Brand Kits' },
+            { name: 'News & Updates' },
           ].map((item, index) => (
             <Link
               key={index}
               href='#'
-              className='block text-gray-800 py-3'
+              className='block text-gray-800 py-3 text-lg md:text-xl'
             >
               {item.name}
             </Link>
@@ -27,17 +34,17 @@ export default function Footer() {
           }
         </div>
         <div className='md:w-full w-1/3'>
-          <h2 className='text-xl font-bold'>Explore</h2>
+          <h2 className='md:text-2xl text-xl font-bold'>Explore</h2>
           {[
-            {name: 'Documentation'},
-            {name: 'Block Explorer'},
-            {name: 'Github'},
-            {name: 'Community'},
+            { name: 'Documentation' },
+            { name: 'Block Explorer' },
+            { name: 'Github' },
+            { name: 'Community' },
           ].map((item, index) => (
             <Link
               key={index}
               href='#'
-              className='block text-gray-800 py-3'
+              className='block text-gray-800 py-3 text-lg md:text-xl'
             >
               {item.name}
             </Link>
@@ -45,16 +52,16 @@ export default function Footer() {
           }
         </div>
         <div className='md:w-full w-1/3'>
-          <h2 className='text-xl font-bold'>Apply</h2>
+          <h2 className='md:text-2xl text-xl font-bold'>Apply</h2>
           {[
-            {name: 'Dev. Incubator'},
-            {name: 'Career'},
-            {name: 'Ambassador'},
+            { name: 'Dev. Incubator' },
+            { name: 'Career' },
+            { name: 'Ambassador' },
           ].map((item, index) => (
             <Link
               key={index}
               href='#'
-              className='block text-gray-800 py-3'
+              className='block text-gray-800 py-3 text-lg md:text-xl'
             >
               {item.name}
             </Link>
@@ -62,17 +69,17 @@ export default function Footer() {
           }
         </div>
         <div className='md:w-full w-1/3'>
-          <h2 className='text-xl font-bold'>Follow Us</h2>
+          <h2 className='md:text-2xl text-xl font-bold'>Follow Us</h2>
           {[
-            {name: 'Twitter'},
-            {name: 'Medium'},
-            {name: 'Telegram'},
-            {name: 'Discord'},
+            { name: 'Twitter' },
+            { name: 'Medium' },
+            { name: 'Telegram' },
+            { name: 'Discord' },
           ].map((item, index) => (
             <Link
               key={index}
               href='#'
-              className='block text-gray-800 py-3'
+              className='block text-gray-800 py-3 text-lg md:text-xl'
             >
               {item.name}
             </Link>
@@ -80,26 +87,26 @@ export default function Footer() {
           }
         </div>
       </div>
-      <div className='flex md:flex-nowrap flex-wrap pt-6 border-t'>
+      <div className='flex md:flex-nowrap mt-5 flex-wrap pt-6 border-t'>
         <Link
           href='#'
-          className='block whitespace-nowrap text-gray-800 px-5'
+          className='block whitespace-nowrap text-gray-800 px-5 text-lg md:text-xl'
         >
           Privacy Policy
         </Link>
         <Link
           href='#'
-          className='block whitespace-nowrap md:my-0 my-4 text-gray-800 px-5 md:border-x-2'
+          className='block whitespace-nowrap md:my-0 my-4 text-gray-800 px-5 md:border-x-2 text-lg md:text-xl'
         >
           Terms & Conditions
         </Link>
         <Link
           href='#'
-          className='block whitespace-nowrap text-gray-800 px-5'
+          className='block whitespace-nowrap text-gray-800 px-5 text-lg md:text-xl'
         >
           Disclaimer
         </Link>
-        <p className='w-full text-center md:mt-0 mt-16 md:text-end'>
+        <p className='w-full text-center md:mt-0 mt-16 md:text-end text-lg md:text-xl'>
           With love 💙 from Example, Inc
         </p>
       </div>
