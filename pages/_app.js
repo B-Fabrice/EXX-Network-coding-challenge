21e597eb167b77cfb7e9a941520f5605ab0c2b13
B-Fabrice <bizimanafabrice707@gmail.com>
2022-12-26 02:05:31 +0200

    connect to wallet

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,46 @@
+import { useEffect, useState } from 'react'
 import PropType from 'prop-types'
-import { ThemeProvider } from 'next-themes'
 import Layout from 'components/layout/layout'
 import 'styles/globals.css'
-import store from '../store'
-import { Provider } from 'react-redux'
+import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { arbitrum, avalanche, bsc, fantom, mainnet, optimism, polygon } from 'wagmi/chains'
+
+if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
+  throw new Error('You need to provide NEXT_PUBLIC_PROJECT_ID env variable')
+}
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+const chains = [mainnet, polygon, optimism, arbitrum, avalanche, fantom, bsc]
+const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: 'web3Modal', chains }),
+  provider
+})
+
+export const ethereumClient = new EthereumClient(wagmiClient, chains)
 
 function MyApp({ Component, pageProps }) {
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    setReady(true)
+  }, [])
+
   return (
-    <Provider store={store}>
-      <ThemeProvider attribute='class'>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </Provider>
+    <>
+      {ready ?
+        <WagmiConfig client={wagmiClient}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </WagmiConfig>
+        : null}
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
   )
 }
 
